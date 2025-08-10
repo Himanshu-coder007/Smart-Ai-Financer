9037722bcb0509141f12f615713b5cbd8d1f1e9b
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-08-10 22:41:59 +0530

    Cleark setup done

---
+++ app/layout.js
@@ -0,0 +1,38 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Smart Finance AI",
+  description: "One Stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          {/*header */}
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p className="">Made by Himanshu Nagose</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
